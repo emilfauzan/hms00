500f318df19dd474d333239135b9272056b6b331
Emil Fauzan <emilfauzan@gmail.com>
2024-11-05 10:14:19 +0700

    modified responsive home page layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,68 @@
 
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Welcome to NT Hotel</h2>
-        <div class="text-base text-gray-500">
-            <a href="#">NT Hotel</a> | Comforting your stay
-        </div>
-        <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum tempora illum qui nisi aperiam a
-            doloremque rerum numquam delectus nesciunt in corporis aspernatur vitae, perferendis odio dignissimos eius
-            veniam!</p>
-        <a href="#">Book your room now! &raquo;</a>
-    </article>
+    <div class="grid md:grid-cols-3 grid-cols-1 gap-3">
+        {{-- Main Article --}}
+        <article class="md:col-span-2 col-span-1 py-8 max-w-screen-md border-b border-gray-300">
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Welcome to NT Hotel</h2>
+            <div class="text-base text-gray-500">
+                <a href="#">NT Hotel</a> | Comforting your stay
+            </div>
+            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum tempora illum
+                qui nisi aperiam a
+                doloremque rerum numquam delectus nesciunt in corporis aspernatur vitae, perferendis odio dignissimos
+                eius
+                veniam!</p>
+            <a href="#">Book your room now! &raquo;</a>
+        </article>
+
+        {{-- Ad --}}
+        <article class="hidden md:block col-span-1 py-8 max-w-screen-md border-b border-gray-300">
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Welcome to NT Hotel</h2>
+            <div class="text-base text-gray-500">
+                <a href="#">NT Hotel</a> | Comforting your stay
+            </div>
+            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum tempora illum
+                qui nisi aperiam a
+                doloremque rerum numquam delectus nesciunt in corporis aspernatur vitae, perferendis odio dignissimos
+                eius
+                veniam!</p>
+            <a href="#">Book your room now! &raquo;</a>
+        </article>
+    </div>
+
+    <div class="grid md:grid-cols-3 grid-cols-1 gap-3">
+        {{-- Main Article --}}
+        <article class="md:col-span-2 col-span-1 py-8 max-w-screen-md border-b border-gray-300">
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Welcome to NT Hotel</h2>
+            <div class="text-base text-gray-500">
+                <a href="#">NT Hotel</a> | Comforting your stay
+            </div>
+            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum tempora illum
+                qui nisi aperiam a
+                doloremque rerum numquam delectus nesciunt in corporis aspernatur vitae, perferendis odio dignissimos
+                eius
+                veniam!</p>
+            <a href="#">Book your room now! &raquo;</a>
+        </article>
+
+        {{-- Ad --}}
+        <article class="hidden md:block col-span-1 py-8 max-w-screen-md border-b border-gray-300">
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Welcome to NT Hotel</h2>
+            <div class="text-base text-gray-500">
+                <a href="#">NT Hotel</a> | Comforting your stay
+            </div>
+            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum tempora illum
+                qui nisi aperiam a
+                doloremque rerum numquam delectus nesciunt in corporis aspernatur vitae, perferendis odio dignissimos
+                eius
+                veniam!</p>
+            <a href="#">Book your room now! &raquo;</a>
+        </article>
+    </div>
+
+    <div class="">
+
+    </div>
 
 </x-layout>
